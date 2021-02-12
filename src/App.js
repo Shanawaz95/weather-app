3f90e482dc48c8b2a371da8abfa3d37e09807f01
Shanawaz95 <shanawaz.a.1995@gmail.com>
2021-02-12 16:31:53 +0530

    changed http to https

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const fetchWeather = () => {
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=4c496af33d732297c30b641b3b235e47`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=4c496af33d732297c30b641b3b235e47`
     )
       .then((res) => res.json())
       .then((result) => {
